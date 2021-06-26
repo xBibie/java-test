2ab9ee2d6574b75a1fc823b264898281588cd23e
Bibi <84073518+xBibie@users.noreply.github.com>
2021-06-26 15:45:24 +0200

    Add files via upload

---
+++ opdracht_009/test.js
@@ -0,0 +1,21 @@
+const age= 18;
+const isFemale = Boolean
+const driverStatus = bob
+if (age >18){
+    console.log('you are 18+, you can have come in');
+}
+else (age <18){
+    console.log('too young, too bad')
+}
+if (isFemale = true){
+    console.log ('yay ladies')
+}
+else (isFemale){
+    console.log('Boo, beer for you')
+}
+if (driverStatus = true){
+    console.log ('you can drive buddy')
+}
+else (driverStatus = false){
+    console.log ('too bad buddy')
+}
